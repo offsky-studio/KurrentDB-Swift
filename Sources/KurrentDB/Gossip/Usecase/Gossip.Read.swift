ffcf9e9bed64e34ab4380bba1d65e33c73cd0b0c
Grady Ghuo <gradyzhuo@gmail.com>
2025-04-19 22:34:16 +0800

    [UPDATE] refactor MemberInfo

--- Sources/KurrentDB/Gossip/Usecase/Gossip.Read.swift
+++ Sources/KurrentDB/Gossip/Usecase/Gossip.Read.swift
@@ -14,7 +14,7 @@ extension Gossip {
         package typealias ServiceClient = UnderlyingClient
         package typealias UnderlyingRequest = ServiceClient.UnderlyingService.Method.Read.Input
         package typealias UnderlyingResponse = ServiceClient.UnderlyingService.Method.Read.Output
-        public typealias Response = [Read.MemberInfo]
+        public typealias Response = [MemberInfo]
 
         package func send(client: ServiceClient, request: ClientRequest<UnderlyingRequest>, callOptions: CallOptions) async throws -> Response {
             try await client.read(request: request, options: callOptions) {
@@ -26,86 +26,3 @@ extension Gossip {
     }
 }
 
-extension Gossip.Read {
-    public enum NodeState: Sendable {
-        package typealias UnderlyingMessage = EventStore_Client_Gossip_MemberInfo.VNodeState
-
-        case initializing
-        case discoverLeader
-        case unknown
-        case preReplica
-        case catchingUp
-        case clone
-        case follower
-        case preLeader
-        case leader
-        case manager
-        case shuttingDown
-        case shutdown
-        case readOnlyLeaderless
-        case preReadOnlyReplica
-        case readOnlyReplica
-        case resigningLeader
-        case UNRECOGNIZED(Int)
-
-        package init(from message: UnderlyingMessage) {
-            switch message {
-            case .initializing:
-                self = .initializing
-            case .discoverLeader:
-                self = .discoverLeader
-            case .unknown:
-                self = .unknown
-            case .preReplica:
-                self = .preReplica
-            case .catchingUp:
-                self = .catchingUp
-            case .clone:
-                self = .clone
-            case .follower:
-                self = .follower
-            case .preLeader:
-                self = .preLeader
-            case .leader:
-                self = .leader
-            case .manager:
-                self = .manager
-            case .shuttingDown:
-                self = .shuttingDown
-            case .shutdown:
-                self = .shutdown
-            case .readOnlyLeaderless:
-                self = .readOnlyLeaderless
-            case .preReadOnlyReplica:
-                self = .preReadOnlyReplica
-            case .readOnlyReplica:
-                self = .readOnlyReplica
-            case .resigningLeader:
-                self = .resigningLeader
-            case let .UNRECOGNIZED(enumValue):
-                self = .UNRECOGNIZED(enumValue)
-            }
-        }
-    }
-
-    public struct MemberInfo: GRPCResponse {
-        package typealias UnderlyingMessage = EventStore_Client_Gossip_MemberInfo
-
-        public let instanceId: UUID
-        public let timeStamp: TimeInterval
-        public let state: NodeState
-        public let isAlive: Bool
-        public let httpEndPoint: Endpoint
-
-        package init(from message: UnderlyingMessage) throws(KurrentError) {
-            guard let uuid = message.instanceID.toUUID() else {
-                throw .initializationError(reason: "MemberInfo can't convert an UUID from message.instanceID: \(message.instanceID)")
-            }
-            instanceId = uuid
-            timeStamp = TimeInterval(message.timeStamp)
-            state = .init(from: message.state)
-            isAlive = message.isAlive
-            httpEndPoint = .init(host: message.httpEndPoint.address, port: message.httpEndPoint.port)
-        }
-    }
-}
---
+++ Sources/KurrentDB/Gossip/VNodeState.swift
@@ -0,0 +1,64 @@
+import GRPCEncapsulates
+
+extension Gossip {
+    public enum VNodeState: Sendable {
+        package typealias UnderlyingMessage = EventStore_Client_Gossip_MemberInfo.VNodeState
+
+        case initializing
+        case discoverLeader
+        case unknown
+        case preReplica
+        case catchingUp
+        case clone
+        case follower
+        case preLeader
+        case leader
+        case manager
+        case shuttingDown
+        case shutdown
+        case readOnlyLeaderless
+        case preReadOnlyReplica
+        case readOnlyReplica
+        case resigningLeader
+        case UNRECOGNIZED(Int)
+
+        package init(from message: UnderlyingMessage) {
+            switch message {
+            case .initializing:
+                self = .initializing
+            case .discoverLeader:
+                self = .discoverLeader
+            case .unknown:
+                self = .unknown
+            case .preReplica:
+                self = .preReplica
+            case .catchingUp:
+                self = .catchingUp
+            case .clone:
+                self = .clone
+            case .follower:
+                self = .follower
+            case .preLeader:
+                self = .preLeader
+            case .leader:
+                self = .leader
+            case .manager:
+                self = .manager
+            case .shuttingDown:
+                self = .shuttingDown
+            case .shutdown:
+                self = .shutdown
+            case .readOnlyLeaderless:
+                self = .readOnlyLeaderless
+            case .preReadOnlyReplica:
+                self = .preReadOnlyReplica
+            case .readOnlyReplica:
+                self = .readOnlyReplica
+            case .resigningLeader:
+                self = .resigningLeader
+            case let .UNRECOGNIZED(enumValue):
+                self = .UNRECOGNIZED(enumValue)
+            }
+        }
+    }
+}
