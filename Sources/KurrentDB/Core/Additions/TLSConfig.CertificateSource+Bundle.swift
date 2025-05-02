b882f6e8e26d89a38e74362d7edc6c6ded72f60c
Grady Ghuo <gradyzhuo@gmail.com>
2025-05-03 02:33:25 +0800

    [UPDATE] add convenience method of TLSConfig.CertificateSource

---
+++ Sources/KurrentDB/Core/Additions/TLSConfig.CertificateSource+Bundle.swift
@@ -0,0 +1,26 @@
+//
+//  NIOSSLTrustRoots+Additions.swift
+//  KurrentCore
+//
+//  Created by Grady Zhuo on 2024/1/1.
+//
+
+import Foundation
+import GRPCNIOTransportCore
+
+extension TLSConfig.CertificateSource {
+    internal static func fileInBundle(forResource resourceName: String, withExtension extenionName: String, format: TLSConfig.SerializationFormat, inDirectory directory: String? = nil, inBundle bundle: Bundle = .main) -> Self? {
+        guard let resourcePath = bundle.path(forResource: resourceName, ofType: extenionName, inDirectory: directory) else {
+            return nil
+        }
+        return .file(path: resourcePath, format: format)
+    }
+
+    public static func crtInBundle(_ fileName: String, inDirectory directory: String? = nil, inBundle bundle: Bundle = .main) -> Self? {
+        .fileInBundle(forResource: fileName, withExtension: "crt", format: .pem, inDirectory: directory, inBundle: bundle)
+    }
+
+    public static func pemInBundle(_ fileName: String, inDirectory directory: String? = nil, inBundle bundle: Bundle = .main) -> Self? {
+        .fileInBundle(forResource: fileName, withExtension: "pem", format: .pem, inDirectory: directory, inBundle: bundle)
+    }
+}
